0385fb3b3cfebc641eab7f3a15e358d712bc0913
wangfeng35 <601948108@qq.com>
2020-05-29 18:37:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,9 @@
 # Modify default IP
 sed -i 's/192.168.2.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-echo '添加Clash'
-git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash
-echo 'CONFIG_PACKAGE_luci-app-clash=y' >> .config
+# echo '添加Clash'
+# git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash
+# echo 'CONFIG_PACKAGE_luci-app-clash=y' >> .config
 
 echo '添加Passwall'
 echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config

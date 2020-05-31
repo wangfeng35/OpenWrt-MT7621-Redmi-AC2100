1e00d4cc43202d2a8d80edbee4c2d7c2faefb802
wangfeng35 <601948108@qq.com>
2020-05-31 23:32:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.2.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # echo '添加Clash'
 # git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash

dbdf320806f3a7a83b8b0139e606c2b9e5aa467b
wangfeng35 <601948108@qq.com>
2020-07-07 17:13:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+# 增加lean的ssr-plus
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+
+# 增加Lienol大神软件包 passwall及其依赖
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 # 增加Kenzok8的软件库 包含passwall及其依赖
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+#echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 # ./scripts/feeds update -a && ./scripts/feeds install -a

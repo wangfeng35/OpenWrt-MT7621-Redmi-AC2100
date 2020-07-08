1bbe2b6ae816482365a435b059f403a80fd8760a
wangfeng35 <601948108@qq.com>
2020-07-08 13:37:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,21 +9,22 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.3.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # echo '添加Clash'
 # git clone https://github.com/frainzy1477/luci-app-clash package/lean/luci-app-clash
 # echo 'CONFIG_PACKAGE_luci-app-clash=y' >> .config
 
-echo '添加Passwall'
-echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=n' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=n' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=n' >> .config
-echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=n' >> .config
-echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
+# .config文件已经包含了
+# echo '添加Passwall'
+# echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=n' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=n' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=n' >> .config
+# echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=n' >> .config
+# echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
 
 # echo '添加luci-app-vssr'
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
